4e950bfb2375258b25328e6baf9fc4fc55e0644e
SpringSunYY <3217778348@qq.com>
2026-07-22 20:26:01 +0800

    部署配置

--- apps/web-antd/src/router/routes/modules/dashboard.ts
+++ apps/web-antd/src/router/routes/modules/dashboard.ts
@@ -1,39 +1,39 @@
 import type { RouteRecordRaw } from 'vue-router';
 
 const routes: RouteRecordRaw[] = [
-  // {
-  //   meta: {
-  //     icon: 'lucide:layout-dashboard',
-  //     order: -1,
-  //     i18n: 'ui.dashboard.title.menu',
-  //     title: '概况',
-  //   },
-  //   name: 'Dashboard',
-  //   path: '/dashboard',
-  //   children: [
-  //     {
-  //       name: 'Workspace',
-  //       path: '/workspace',
-  //       component: () => import('#/views/dashboard/workspace/index.vue'),
-  //       meta: {
-  //         icon: 'carbon:workspace',
-  //         i18n: 'ui.dashboard.workspace.menu',
-  //         title: '工作台',
-  //       },
-  //     },
-  // {
-  //   name: 'Analytics',
-  //   path: '/analytics',
-  //   component: () => import('#/views/dashboard/analytics/index.vue'),
-  //   meta: {
-  //     affixTab: true,
-  //     icon: 'lucide:area-chart',
-  //     i18n: 'ui.dashboard.analytics.menu',
-  //     title: '分析页',
-  //   },
-  // },
-  //   ],
-  // },
+  {
+    meta: {
+      icon: 'lucide:layout-dashboard',
+      order: -1,
+      i18n: 'ui.dashboard.title.menu',
+      title: '概况',
+    },
+    name: 'Dashboard',
+    path: '/dashboard',
+    children: [
+      {
+        name: 'Workspace',
+        path: '/workspace',
+        component: () => import('#/views/dashboard/workspace/index.vue'),
+        meta: {
+          icon: 'carbon:workspace',
+          i18n: 'ui.dashboard.workspace.menu',
+          title: '工作台',
+        },
+      },
+      // {
+      //   name: 'Analytics',
+      //   path: '/analytics',
+      //   component: () => import('#/views/dashboard/analytics/index.vue'),
+      //   meta: {
+      //     affixTab: true,
+      //     icon: 'lucide:area-chart',
+      //     i18n: 'ui.dashboard.analytics.menu',
+      //     title: '分析页',
+      //   },
+      // },
+    ],
+  },
   {
     name: 'Profile',
     path: '/profile',
